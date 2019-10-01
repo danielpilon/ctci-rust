c8bbdbe67bc4bd6d9f395c3c89dded41402a97a6
danielpilon <danielpilon@gmail.com>
2019-09-30 23:55:34 -0300

    Created project; Exercise 01 solution

---
+++ src/ch01/e01_isunique.rs
@@ -0,0 +1,44 @@
+use std::collections::HashSet;
+
+pub fn is_unique(_s: String) -> bool {
+    let len = _s.len();
+    if len > 128 {
+        return false;
+    }
+
+    let mut chars = HashSet::new();
+
+    for c in _s.chars() {
+        if chars.contains(&c) {
+            return false;
+        } else {
+            chars.insert(c);
+        }
+    }
+
+    return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_is_unique() {
+        assert_eq!(is_unique(String::from("Daniel")), true);
+    }
+
+    #[test]
+    fn it_is_not_unique() {
+        assert_eq!(is_unique(String::from("Daniela")), false);
+    }
+
+    #[test]
+    fn it_is_unique_when_empty() {
+        assert_eq!(is_unique(String::from("")), true);
+    }
+
+    #[test]
+    fn it_is_not_unique_when_has_more_than_128_chars() {
+        assert_eq!(is_unique(String::from("gxSJ2nXDda5WiRq6ow4HNGpxfvS4AzMi2im7UjmfZ3kwGtNf69d69uGns6dL12qeP664hP5sjml85JlxwtA05vRhkK2XnXK3EPPsqHuJmvvkrZByHvYXVV83n7YxcN2h9")), false);
+    }
+}
---
+++ src/ch01/mod.rs
@@ -0,0 +1,1 @@
+mod e01_isunique;
